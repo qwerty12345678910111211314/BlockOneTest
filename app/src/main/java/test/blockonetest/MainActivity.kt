ce4b49e4222da0b85001a3ef9740d5b5c281515b
David Martin <david@eksdyne.com>
2018-08-11 17:57:47 -0400

    Added button view.

--- app/src/main/java/test/blockonetest/MainActivity.kt
+++ app/src/main/java/test/blockonetest/MainActivity.kt
@@ -1,12 +1,19 @@
 package test.blockonetest
 
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fm = this.supportFragmentManager
+        val ft2 = fm!!.beginTransaction()
+        ft2.replace(R.id.content, MenuView())
+        ft2.addToBackStack(null)
+        ft2.commit()
     }
 }
---
+++ app/src/main/java/test/blockonetest/MenuView.kt
@@ -0,0 +1,26 @@
+package test.blockonetest
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class MenuView : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater!!.inflate(R.layout.menu_view, container, false)
+
+        val blockButton = view.findViewById<Button>(R.id.show_blocks)
+        blockButton.setOnClickListener{
+            val fm = this.fragmentManager
+            val ft2 = fm!!.beginTransaction()
+            var list = BlockListView()
+            ft2.replace(R.id.content, list)
+            ft2.addToBackStack(null)
+            ft2.commit()
+        }
+        return view
+    }
+}
